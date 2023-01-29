078fa89c375c484507261510775142d391382b13
Max Jdanov <maxzhdanov@list.ru>
2023-01-29 12:58:09 +0300

    task4 has been done

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.Clear();
+    Console.Write("введите число1: ");
+    int number1 = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+    Console.Write("введите число2: ");
+    int number2 = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+    Console.Write("введите число3: ");
+    int number3 = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+    if (number1 > number2)
+    {
+      if (number1 > number3)
+        {
+          Console.WriteLine(number1);
+        }
+      else
+      {
+        Console.WriteLine(number3);
+      }
+        
+      
+    
+    }
+    else
+    {
+      if (number2 > number3)
+      {
+        Console.WriteLine(number2);
+      }
+      else
+      {
+        Console.WriteLine(number3);
+      }
+    }
+  }
+}
