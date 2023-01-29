8cf7dca493daf2d61f53a9f3da1316551f569b2c
Max Jdanov <maxzhdanov@list.ru>
2023-01-29 15:31:34 +0300

    task8 has been done

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.Clear();
+    Console.Write("введите число: ");
+    int N = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+    int S = 2;
+    if (N > 1)
+    {
+      while (S <= N)
+      if (S%2==0)
+      {
+        Console.WriteLine(S++);
+      }
+      else
+      {
+        Console.WriteLine(" ");
+        S= S + 1;
+      }
+    
+    }
+    else
+    {
+      Console.WriteLine("negative");
+    }
+  }
+}
