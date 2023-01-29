1c3a34ec4c2b944a5918368d904c9501d2ee78e9
Max Jdanov <maxzhdanov@list.ru>
2023-01-29 12:46:16 +0300

    task2 has been done

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.Clear();
+    Console.Write("введите число1: ");
+    int number1 = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+    Console.Write("введите число2: ");
+    int number2 = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+    if (number1 > number2)
+    {
+      Console.WriteLine("max = " + number1 + " min = " + number2);
+    
+    }
+    else
+    {
+      Console.WriteLine("max = " + number2 + " min = " + number1);
+    }
+  }
+}
