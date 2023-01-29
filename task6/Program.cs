50f3ba93ce999fa11089815c4bf85609656d898b
Max Jdanov <maxzhdanov@list.ru>
2023-01-29 13:03:57 +0300

    task6 has been done

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.Clear();
+    Console.Write("введите число: ");
+    int number = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+    if (number%2 == 0)
+    {
+      Console.WriteLine("yes");
+    
+    }
+    else
+    {
+      Console.WriteLine("no");
+    }
+  }
+}
