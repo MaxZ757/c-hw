8b410ffd19b08c74682067560d44cb48b5dfd4bd
Max Jdanov <maxzhdanov@list.ru>
2023-02-01 22:55:15 +0300

    task10 has been done

---
+++ task10/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.Clear();
+    Console.Write("введите трехзгачное число: ");
+    int number = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+    int a1 = number / 10;
+    int a2 = a1 % 10;
+    Console.WriteLine(a2);
+    
+  }
+}
+
