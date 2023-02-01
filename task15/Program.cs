ef1278c8a512486fdb7fc8ab5f1dc9f24b1ff1e2
Max Jdanov <maxzhdanov@list.ru>
2023-02-01 23:21:36 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.Clear();
+    Console.Write("введите номер дня недели от 1 до 7 (пн-вск) ");
+    int number = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+    if (number >= 1 & number <=5)
+    {
+      Console.WriteLine($" {number}  -> нет");
+    }
+    else
+    {
+      if (number == 6  || number == 7)
+      {
+        Console.WriteLine($" {number}  -> да");
+      }
+      else
+      {
+        Console.WriteLine($" не то число ");
+      }
+
+    }
+    
+  }
+}
+
