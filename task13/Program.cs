5cd86edb3cdccdb430c28efb82ddf69d35e31b90
Max Jdanov <maxzhdanov@list.ru>
2023-02-02 18:03:01 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.Clear();
+    Console.Write(" введите любое число ");
+    int number = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+    int ind = 2;
+    if (number>99)
+    {
+      int third = number.ToString()[ind] - '0';
+      Console.WriteLine(third);
+    }
+    else
+    {
+      Console.WriteLine("третьей цифры нет");
+    }
+    
+  }
+}
+
