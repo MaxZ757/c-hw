85729d64b5a3d9cb2e1d2bf02171423527109069
Max Jdanov <maxzhdanov@list.ru>
2023-02-05 16:55:31 +0300

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.Clear();
+    Console.Write(" введите целое число ");
+    int N = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+    int counter = 1;
+    while (counter < N)
+    {
+      Console.Write((Math.Pow(counter,3)) + ", ");
+      counter++;
+    }
+    Console.WriteLine((Math.Pow(counter,3)) + ", ");
+  }
+}
+
+
+
