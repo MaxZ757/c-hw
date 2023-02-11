30a305ac681f961153e958ba3b3f1249c62050d2
Max Jdanov <maxzhdanov@list.ru>
2023-02-11 14:18:05 +0300

    29

---
+++ task29/Program.cs
@@ -0,0 +1,32 @@
+﻿int Pr(string mes)
+{
+  System.Console.Write(mes);
+  string ReadInput = System.Console.ReadLine();
+  int result = int.Parse(ReadInput);
+  return result;
+}
+
+int[] GenArr(int length , int minValue, int maxValue)
+{
+  int[] array = new int[length];
+  Random random = new Random();
+  for (int i = 0; i < length; i++)
+  {
+    array[i] = random.Next(minValue, maxValue + 1);
+  }
+  return array;
+}
+
+void PrintArr(int[] array)
+{
+  System.Console.Write("i");
+  for (int i = 0; i < array.Length - 1; i++)
+  {
+    System.Console.Write ($"{array[i]}");
+  }
+  System.Console.Write ($"{array[array.Length - 1]}");
+  System.Console.Write ("i");
+}
+
+
+PrintArr(array);
