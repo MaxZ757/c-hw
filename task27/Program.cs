ce5151cd4b2a45ae2374f0b921c0bb918d8e3809
Max Jdanov <maxzhdanov@list.ru>
2023-02-11 14:17:47 +0300

    25

---
+++ task27/Program.cs
@@ -0,0 +1,25 @@
+﻿
+    Console.Clear();
+    Console.Write(" введите целое число ");
+    int n = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+   
+    Console.WriteLine($"Сумма числа {GetSum(n)}");
+   int GetSum(int num)
+    {
+      int count = Convert.ToString(num).Length;
+      int temp = 0;
+      int result = 0;
+      for (int i = 0; i <= count; i++)
+      {
+        temp = num - num % 10;
+        result = result + (num - temp);
+        num = num / 10;
+
+      }
+      return result; 
+    }
+    
+
+
+
+
