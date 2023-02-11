06fdf5256e1a818bbc45a1ea4a3bc3421ab720c2
Max Jdanov <maxzhdanov@list.ru>
2023-02-11 14:16:50 +0300

    25

---
+++ task25/Program.cs
@@ -0,0 +1,23 @@
+﻿
+    internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.Clear();
+    Console.Write(" введите целое число1 ");
+    int N1 = int.Parse(Console.ReadLine()); // перевод из строковой в числовой формат
+    Console.Write(" введите целое число2 ");
+    int N2 = int.Parse(Console.ReadLine());
+    double power = Math.Pow(N1,N2);
+    Console.WriteLine($"Возводим в степень и получаем {power}");
+  }
+
+}  
+   
+    
+    
+   
+
+  
+
+
