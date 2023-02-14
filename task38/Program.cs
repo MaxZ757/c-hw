905dc022b27e6a2c9b2a86e28ed0dad0af6452b8
Max Jdanov <maxzhdanov@list.ru>
2023-02-14 22:23:45 +0300

    task38

---
+++ task38/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine());
+double[] numbers = new double[size];
+RandomNum(numbers);
+Console.WriteLine("Рандомный массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (numbers[j] > max)
+        {
+            max = numbers[j];
+        }
+    if (numbers[j] < min)
+        {
+            min = numbers[j];
+        }
+}
+
+Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между макс и мин значениями = {Math.Round((max - min), 1)}");
+
+void RandomNum(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 10;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
