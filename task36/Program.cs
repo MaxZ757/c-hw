b774096c1a5b8d9ea6d7ec9bfb907ca822ad6397
Max Jdanov <maxzhdanov@list.ru>
2023-02-14 22:23:21 +0300

    task36

---
+++ task36/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine());
+int[] numbers = new int[size];
+RandomNum(numbers);
+Console.WriteLine("Рандомный массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int j = 0; j < numbers.Length; j+=2)
+    sum += numbers[j];
+
+    Console.WriteLine($"всего {numbers.Length} чисел, сумма нечетных элементов  = {sum}");
+
+void RandomNum(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
